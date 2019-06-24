0b44ab1cf22d973712170eebeb62ac38c81277c6
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-06-24 19:05:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+  return Math.abs(pickup - 42);
+}
+
+function distanceFromHqInFeet(pickup) {
+  return distanceFromHqInBlocks(pickup) * 264;
+}
+
+function distanceTravelledInFeet(pickup, end) {
+  return Math.abs(pickup-end) * 264;
+}
+
+function calculatesFarePrice(pickup, end ) {
+  let distance = distanceTravelledInFeet(pickup, end)
+
+  if(distance < 2000) {
+    return (Math.max(distance-400, 0)) * .02;
+  } else if (distance < 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
