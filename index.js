f1f861b0bee66a6611241ef62d587bb0fd8d2a38
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-06-24 18:58:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks() {
-
+function distanceFromHqInBlocks(pickup) {
+  return Math.abs(pickup - 42)
 }
 
 function distanceFromHqInFeet() {
-  
+
 }
 
 function distanceTravelledInFeet() {
