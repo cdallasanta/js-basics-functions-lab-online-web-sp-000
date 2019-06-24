3d3c326632571db2257b76fb284635a6b020c1e3
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-06-24 19:03:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,11 +13,18 @@ function distanceTravelledInFeet(pickup, end) {
 
 function calculatesFarePrice(pickup, end ) {
   let ditance = distanceTravelledInFeet(pickup, end)
-  
+
+  if(distance < 2000) {
+    return (distance-400) * .02;
+  } else if (distance < 2500) {
+    return 25;
+  } else {
+    return ""
+  }
 }
 
 For a distance between 400 and 2000 feet,
 the price is 2 cents per foot (not including 400, which are free!).
-Then Scuber charges a flat fare for a distance over 2000 feet and under 2500 feet.
+Then Scuber charges $25 for a distance over 2000 feet and under 2500 feet.
 Finally, Scuber does not allow any rides over 2500 feet
 the function returns 'cannot travel that far' if a ride over 2500 feet is requested.
